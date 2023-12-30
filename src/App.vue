362392471061e393daddf901f91f3179777c35d2
Alexander Svetlichny <alexsvetlichny29@gmail.com>
2023-12-31 01:18:14 +0300

    lesson-1-reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,74 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const todos = ref<{ id: string; title: string; description: string }[]>([]);
+const title = ref("");
+const description = ref("");
+const handleAddTodo = () => {
+	if (title.value && description.value) {
+		todos.value.push({
+			id: new Date().toISOString(),
+			description: description.value,
+			title: title.value,
+		});
+		title.value = "";
+		description.value = "";
+	}
+};
+const handleDelete = (id: string) => {
+	todos.value = todos.value.filter((todo) => todo.id !== id);
+};
+</script>
 
 <template>
-	<div>Mort</div>
+	<div class="main">
+		<div class="controls">
+			<input placeholder="title" v-model="title" />
+			<input placeholder="description" v-model="description" />
+			<button @click="handleAddTodo">Add to todo</button>
+		</div>
+		<h2>Todos</h2>
+		<div class="todos">
+			<div class="todo" v-for="todo in todos">
+				<span>Title:{{ todo.title }}</span>
+				<span>Description:{{ todo.description }}</span>
+				<button @click="handleDelete(todo.id)">Delete</button>
+			</div>
+		</div>
+	</div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+button {
+	color: white;
+	background-color: rgb(153, 201, 83);
+	padding: 5px;
+	border: none;
+	border-radius: 5px;
+}
+.main {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+.controls {
+	display: flex;
+	gap: 10px;
+}
+.todos {
+	display: flex;
+	flex-direction: column;
+	gap: 6px;
+	background-color: rgb(253, 246, 242);
+	padding: 5px 20px;
+	border-radius: 5px;
+	.todo {
+		display: flex;
+
+		flex-direction: column;
+		border: 1px solid rgb(207, 83, 26);
+		background-color: rgb(189, 207, 233);
+		padding: 10px;
+		border-radius: 10px;
+	}
+}
+</style>
